2ae933777bc527aa50e7551a079641903f6fb70a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-04-25 05:59:41 -0400

    Add Zhang MMSF dataset

--- R/parse-others.R
+++ R/parse-others.R
@@ -17,3 +17,23 @@ parse_d20190415_HF068 <- function(path, UTC_offset) {
   dat$Error <- FALSE
   dat
 }
+
+#' Parse a file with timestamp in year-doy-hour format.
+#'
+#' @param path Data directory path, character
+#' @param UTC_offset Offset from UTC in hours, numeric
+#' @return A \code{data.frame} containing extracted data.
+#' @importFrom utils read.csv
+#' @export
+parse_IRGA_ydh <- function(path, UTC_offset) {
+  files <- list.files(path, pattern = ".csv$", full.names = TRUE, recursive = TRUE)
+  dat <- do.call("rbind", lapply(files, read.csv,
+                                 na.strings = "-9999", stringsAsFactors = FALSE, check.names = FALSE))
+  dat$Timestamp <- as.POSIXct(paste(dat$year, dat$doy, dat$hour),
+                              format = "%Y %j %H", tz = "UTC") - UTC_offset * 60 * 60
+  dat$year <- dat$doy <- dat$hour <- NULL
+  # Fill in data given at https://www.sciencedirect.com/science/article/pii/S0168192312003759
+  dat$Area <- 491
+  dat$Error <- FALSE
+  dat
+}
