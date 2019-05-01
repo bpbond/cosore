4d997030ccdfc58dd9b0c1919925439c9ba6db6f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-04-30 20:59:01 -0400

    Add user function documentation

--- R/user.R
+++ R/user.R
@@ -1,5 +1,11 @@
 # User-side convenience functions
 
+#' Extract one part (\code{description}, \code{data}, etc.) of all datasets.
+#'
+#' @param all All data, a list of lists
+#' @param element Name of table to extract, character
+#' @return A \code{\link{data.frame}}.
+#' @export
 csr_element <- function(all, element) {
 
   # Almost all of this could be replaced by a single `dplyr::bind_rows()` call...
@@ -19,6 +25,12 @@ csr_element <- function(all, element) {
           }), make.row.names = FALSE, stringsAsFactors = FALSE))
 }
 
+#' Extract all the \code{data} tables from the COSORE dataset.
+#'
+#' @param all All data, a list of lists
+#' @return A \code{\link{data.frame}}.
+#' @export
 csr_data <- function(all) {
   csr_element(all, "data")
 }
+
