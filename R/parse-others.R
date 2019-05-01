aa7ce12db68771989dd5194712678ffc88f5d931
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-04-30 20:58:31 -0400

    Change Varner timestamp field

--- R/parse-others.R
+++ R/parse-others.R
@@ -1,4 +1,27 @@
 
+#' Parse a eofFD (forced diffusion) file.
+#'
+#' @param path Data directory path, character
+#' @param UTC_offset Offset from UTC in hours, numeric
+#' @return A \code{data.frame} containing extracted data.
+#' @importFrom utils read.csv
+#' @export
+parse_eosFD <- function(path, UTC_offset) {
+  browser()
+  files <- list.files(path, pattern = ".dat$", full.names = TRUE, recursive = TRUE)
+
+  readfunc <- function(f) {
+    dat <- readLines(f)
+    read.csv(textConnection(dat[c(-1, -3, -4)]), stringsAsFactors = FALSE, check.names = FALSE, na.strings = "NAN")
+  }
+
+  dat <- do.call("rbind", lapply(files, readfunc))
+  dat$TIMESTAMP <- as.POSIXct(dat$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S", tz = "UTC") - UTC_offset * 60 * 60
+  dat$Area <- pi * (10 / 2) ^ 2
+  dat$Error <- FALSE
+  dat
+}
+
 
 #' Parse a file from Harvard Forest HF068 dataset.
 #'
@@ -10,8 +33,8 @@
 parse_d20190415_HF068 <- function(path, UTC_offset) {
   files <- list.files(path, pattern = ".csv$", full.names = TRUE, recursive = TRUE)
   dat <- do.call("rbind", lapply(files, read.csv, stringsAsFactors = FALSE, check.names = FALSE))
-  dat$Timestamp <- as.POSIXct(dat$datetime, format = "%Y-%m-%dT%H:%M", tz = "UTC") - UTC_offset * 60 * 60
-  dat$datetime <- dat$year <- dat$doy <- dat$hour <- NULL
+  dat$datetime <- as.POSIXct(dat$datetime, format = "%Y-%m-%dT%H:%M", tz = "UTC") - UTC_offset * 60 * 60
+  dat$year <- dat$doy <- dat$hour <- NULL
   # Fill in data given at http://harvardforest.fas.harvard.edu:8080/exist/apps/datasets/showData.html?id=hf068
   dat$Area <- 43.2 ^ 2
   dat$Error <- FALSE
