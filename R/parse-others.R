118f39b0ca285fbaedcfb7a7aca1a1959f04c054
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-05-05 06:59:29 -0400

    Add Davidson HF006 datasets
    Closes #19

--- R/parse-others.R
+++ R/parse-others.R
@@ -1,7 +1,58 @@
 
 
-`parse_LI-6252_d20190504_DAVIDSON_hf006-03` <- function(path, offset) {
-  data.frame()
+#' Parse a custom file from d20190504_DAVIDSON_hf006-05.
+#'
+#' @param path Data directory path, character
+#' @param UTC_offset Offset from UTC in hours, numeric
+#' @return A \code{data.frame} containing extracted data.
+#' @importFrom utils read.csv
+#' @export
+`parse_LI-6252_d20190504_DAVIDSON_hf006-05` <- function(path, UTC_offset) {
+  files <- list.files(path, pattern = ".csv$", full.names = TRUE, recursive = TRUE)
+  dat <- do.call("rbind", lapply(files, read.csv, stringsAsFactors = FALSE, check.names = FALSE))
+
+  dat$datetime <- as.POSIXct(dat$datetime, format = "%Y-%m-%dT%H:%M", tz = "UTC") - UTC_offset * 60 * 60
+
+  dat_control <- subset(dat, treatment == "C")
+  dat_control$CSR_T10 <- dat_control$soilt.c
+  dat_control$CSR_SM10 <- dat_control$vsm.c
+  dat_trench <- subset(dat, treatment != "C")
+  dat_trench$CSR_T10 <- dat_trench$soilt.t
+  dat_trench$CSR_SM10 <- dat_trench$vsm.t
+
+  rbind_list(list(dat_control, dat_trench))
+}
+
+
+#' Parse a custom file from d20190504_DAVIDSON_hf006-03.
+#'
+#' @param path Data directory path, character
+#' @param UTC_offset Offset from UTC in hours, numeric
+#' @return A \code{data.frame} containing extracted data.
+#' @importFrom utils read.csv
+#' @export
+`parse_LI-6252_d20190504_DAVIDSON_hf006-03` <- function(path, UTC_offset) {
+  files <- list.files(path, pattern = ".csv$", full.names = TRUE, recursive = TRUE)
+  dat <- do.call("rbind", lapply(files, read.csv, stringsAsFactors = FALSE, check.names = FALSE))
+
+  dat$datetime <- as.POSIXct(dat$datetime, format = "%Y-%m-%dT%H:%M", tz = "UTC") - UTC_offset * 60 * 60
+
+  # Temperature and soil moisture data
+  names(dat) <- gsub("^temp\\.ll", "CSR_T0", names(dat))
+  names(dat) <- gsub("^temp", "CSR_T", names(dat))
+  names(dat) <- gsub("^mois", "CSR_SM", names(dat))
+
+  # Flux fields
+  fluxcols <- grep("^flux", names(dat))
+  x <- dat[-fluxcols]
+  results <- list()
+  for(i in seq_along(fluxcols)) {
+    results[[i]] <- x
+    results[[i]]$flux <- dat[,fluxcols[i]]
+    results[[i]]$CSR_PORT <- i
+  }
+
+  rbind_list(results)
 }
 
 #' Parse a custom eofFD (forced diffusion) file from d20190430_DESAI.
