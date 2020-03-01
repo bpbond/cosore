6d2319ced953472009f7052d570dad5deda6177f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-03-01 08:30:53 -0500

    Test for invalid timestamps and CSR_PORT addition

--- R/parser.R
+++ R/parser.R
@@ -390,12 +390,7 @@ read_raw_dataset <- function(dataset_name, raw_data, dataset) {
     # Remove records with invalid timestamps
     diag$CSR_RECORDS_REMOVED_TIMESTAMP <- sum(ctlist$na_ts)
     diag$CSR_EXAMPLE_BAD_TIMESTAMPS <- ctlist$bad_examples
-    dsd <- dsd[!is.na(dsd$CSR_TIMESTAMP_BEGIN),]
-    dsd <- dsd[!is.na(dsd$CSR_TIMESTAMP_END),]
-
-    if(nrow(dsd) == 0) {
-      stop("Timestamps could not be parsed with ", tf, " and tz ", tz)
-    }
+    dsd <- remove_invalid_timestamps(dsd, tf, tz)
 
     # Change to the site's timezone (which is usually the same but might be different)
     dsd <- lubridate::with_tz(dsd, tzone = dataset$description$CSR_TIMEZONE)
@@ -405,10 +400,7 @@ read_raw_dataset <- function(dataset_name, raw_data, dataset) {
     diag$CSR_TIME_END <- format(max(dsd$CSR_TIMESTAMP_END), format = "%Y-%m-%d")
 
     # Add port column if necessary
-    if(!"CSR_PORT" %in% names(dsd) & nrow(dsd)) {
-      dsd$CSR_PORT <- 0
-    }
-    dsd$CSR_PORT <- convert_to_numeric(dsd$CSR_PORT, "CSR_PORT")
+    dsd <- add_port_column(dsd)
 
     # Rearrange columns
     dsd <- rearrange_columns(dsd, required_cols =
