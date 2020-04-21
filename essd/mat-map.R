06fda76bbe91fa82ebeb08a8d11d61afee5e5e9f
Stephanie Pennington <spenn1@terpmail.umd.edu>
2020-04-21 10:58:57 -0400

    Add comments for clarity

--- essd/mat-map.R
+++ essd/mat-map.R
@@ -1,57 +1,59 @@
+# Script to extract terrestrial MAT and MAP data from WorldClim
+# Created April 2020 | Stephanie Pennington
+
+# Load libraries
 library(raster)
 library(sp)
 library(dplyr)
 library(cosore)
 library(tidyr)
 library(ggplot2)
+library(RColorBrewer)
 
-#download worldclim data for precip and tmean
+# Download worldclim data for precip and tmean, store in essd/ folder
 precip <- getData("worldclim", path = "essd/", var="prec", res = 10)
 tmean <- getData("worldclim", path = "essd/", var = "tmean", res = 10)
 
-#pull out cosore lat and lons
+# Pull out cosore dataset latitudes and longitudes
 csr_table(table = "description") %>%
   select(CSR_LONGITUDE, CSR_LATITUDE) -> cosore_coords
 
-#extract cosore location data
+# Extract cosore location data from worldclim data for precip...
 raster::extract(precip, cosore_coords) -> precip_coords
 apply(precip_coords, 1, mean) -> map_cosore
 cbind(cosore_coords, map_cosore) -> map_coords
 
+# ...and tmean
 raster::extract(tmean, cosore_coords) -> tmean_vals
 apply(tmean_vals, 1, mean) -> mat_cosore
 cbind(map_coords, mat_cosore) -> mat_coords
 
-left_join(map_coords, mat_coords) -> cosore_points
+left_join(map_coords, mat_coords) %>%
+  # Temp data is stored in degC * 10, so we need to divide to get back to degC
+  mutate(mat_cosore = mat_cosore/10) -> cosore_points
 
-#extract climate space data
+# Extract global climate space data
 raster::as.data.frame(precip, xy = TRUE) %>%
   drop_na() -> precip_global
 
+# Calculate annual mean for precip...
 precip_global %>%
   select(-x, -y) %>%
   apply(1, mean) -> map_global
 
-#mat
 raster::as.data.frame(tmean, xy = TRUE) %>%
   drop_na() -> tmean_global
 
+# ...and tmean
 tmean_global %>%
   select(-x, -y) %>%
   apply(1, mean) -> mat_global
 
+# Create tibble with corresponding coordinates
 tibble(x = tmean_global$x, y = tmean_global$y, mat = as.vector(mat_global)) -> mat
-tibble(x = precip_global$x, y = precip_global$y, mat = as.vector(map_global)) -> map
-left_join(map, mat, by = c("x", "y")) -> map_mat_global
+tibble(x = precip_global$x, y = precip_global$y, map = as.vector(map_global)) -> map
 
-ggplot() +
-  geom_hex(data = map_mat_global,
-             aes(x = mat.y/10, y = mat.x), bins = 100) +
-#  scale_color_continuous(pal) +
-  geom_point(data = cosore_points, aes(x = mat_cosore/10, y = map_cosore),
-             color = "white", shape = 4) +
-  theme_minimal() +
-  labs(x = "MAT", y = "MAP")
-
-
+left_join(map, mat, by = c("x", "y")) %>%
+  # Temp data is stored in degC * 10, so we need to divide to get back to degC
+  mutate(mat = mat/10) -> map_mat_global
 
