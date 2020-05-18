eb029a9e7d5deb344da58147f7441e889fc853ed
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-05-18 15:18:56 -0400

    Add CITATION file and startup messages

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+
+.onAttach <- function(libname, pkgname) {
+  tip <- random_tip()
+  packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+}
+
+random_tip <- function() {
+  # Idea from https://github.com/tidyverse/ggplot2/blob/master/R/zzz.r
+  tips <- c(
+    "COSORE data are licensed under CC-BY-4: https://creativecommons.org/licenses/by/4.0/",
+    "Please strive to cite underlying dataset papers - DOIs are listed in csr_database() output",
+    "Type citation(\"cosore\") for the main COSORE database reference"
+  )
+
+  sample(tips, 1)
+}
